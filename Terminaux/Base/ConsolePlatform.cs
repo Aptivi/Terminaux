3c27d6168bafc0b4a3fb7c409101fd13a671b24a
Aptivi CEO <eoflaoevicecity@gmail.com>
2023-12-02 18:57:49 +0300

    upd - Use SpecProbe.Software
    ---
    
    Type: upd
    Breaking: False
    Doc Required: False
    Part: 1/1

--- Terminaux/Base/ConsolePlatform.cs
+++ Terminaux/Base/ConsolePlatform.cs
@@ -16,8 +16,8 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+using SpecProbe.Software.Kernel;
 using System;
-using UnameNET;
 
 namespace Terminaux.Base
 {
