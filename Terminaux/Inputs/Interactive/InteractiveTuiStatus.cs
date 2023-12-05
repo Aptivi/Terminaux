00d0d464c72919d3749f5a7a4472f0fcc4c692cb
Aptivi CEO <eoflaoevicecity@gmail.com>
2023-12-05 13:52:41 +0300

    imp - Synchronized the default TUI colors with N-KS
    ---
    
    The default TUI colors now match the Nitrocid KS defaults.
    
    ---
    
    Type: imp
    Breaking: False
    Doc Required: False
    Part: 1/1

--- Terminaux/Inputs/Interactive/InteractiveTuiStatus.cs
+++ Terminaux/Inputs/Interactive/InteractiveTuiStatus.cs
@@ -16,25 +16,6 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-
-
-// Terminaux  Copyright (C) 2023  Aptivi
-// 
-// This file is part of Terminaux
-// 
-// Terminaux is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-// 
-// Terminaux is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-// 
-// You should have received a copy of the GNU General Public License
-// along with this program.  If not, see <https://www.gnu.org/licenses/>.
-
 using Terminaux.Colors;
 
 namespace Terminaux.Inputs.Interactive
@@ -61,7 +42,7 @@ namespace Terminaux.Inputs.Interactive
         /// <summary>
         /// Interactive TUI background color
         /// </summary>
-        public static Color BackgroundColor { get; set; } = "4";
+        public static Color BackgroundColor { get; set; } = "0";
         /// <summary>
         /// Interactive TUI foreground color
         /// </summary>
@@ -69,7 +50,7 @@ namespace Terminaux.Inputs.Interactive
         /// <summary>
         /// Interactive TUI pane background color
         /// </summary>
-        public static Color PaneBackgroundColor { get; set; } = "12";
+        public static Color PaneBackgroundColor { get; set; } = "0";
         /// <summary>
         /// Interactive TUI pane separator color
         /// </summary>
@@ -81,11 +62,11 @@ namespace Terminaux.Inputs.Interactive
         /// <summary>
         /// Interactive TUI pane selected item color (foreground)
         /// </summary>
-        public static Color PaneSelectedItemForeColor { get; set; } = "11";
+        public static Color PaneSelectedItemForeColor { get; set; } = "0";
         /// <summary>
         /// Interactive TUI pane selected item color (background)
         /// </summary>
-        public static Color PaneSelectedItemBackColor { get; set; } = "4";
+        public static Color PaneSelectedItemBackColor { get; set; } = "3";
         /// <summary>
         /// Interactive TUI pane item color (foreground)
         /// </summary>
@@ -93,19 +74,19 @@ namespace Terminaux.Inputs.Interactive
         /// <summary>
         /// Interactive TUI pane item color (background)
         /// </summary>
-        public static Color PaneItemBackColor { get; set; } = "12";
+        public static Color PaneItemBackColor { get; set; } = "0";
         /// <summary>
         /// Interactive TUI option background color
         /// </summary>
-        public static Color OptionBackgroundColor { get; set; } = "6";
+        public static Color OptionBackgroundColor { get; set; } = "3";
         /// <summary>
         /// Interactive TUI key binding in option color
         /// </summary>
-        public static Color KeyBindingOptionColor { get; set; } = "15";
+        public static Color KeyBindingOptionColor { get; set; } = "0";
         /// <summary>
         /// Interactive TUI option foreground color
         /// </summary>
-        public static Color OptionForegroundColor { get; set; } = "14";
+        public static Color OptionForegroundColor { get; set; } = "11";
         /// <summary>
         /// Interactive TUI box background color
         /// </summary>
