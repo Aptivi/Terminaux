3b67e1b28e2313e34a6425e2d00466cbf4b86c0d
Aptivi CEO <eoflaoevicecity@gmail.com>
2024-01-09 10:31:11 +0300

    chg - Added resize listener as part of pack process
    ---
    
    We've added Terminaux's resize listener library as part of the packing process.
    
    ---
    
    Type: chg
    Breaking: False
    Doc Required: False
    Part: 1/1

--- tools/pack.cmd
+++ tools/pack.cmd
@@ -24,8 +24,8 @@ if "%releaseconfig%" == "" set releaseconfig=Release
 :packbin
 echo Packing binary...
 "%ProgramFiles%\7-Zip\7z.exe" a -tzip %temp%/%version%-bin.zip "..\Terminaux\bin\%releaseconfig%\netstandard2.0\*"
-"%ProgramFiles%\7-Zip\7z.exe" a -tzip %temp%/%version%-bin-res-48.zip "..\Terminaux\bin\%releaseconfig%\net48\*"
-"%ProgramFiles%\7-Zip\7z.exe" a -tzip %temp%/%version%-bin-res-8.zip "..\Terminaux\bin\%releaseconfig%\net8.0\*"
+"%ProgramFiles%\7-Zip\7z.exe" a -tzip %temp%/%version%-bin-res-48.zip "..\Terminaux.ResizeListener\bin\%releaseconfig%\net48\*"
+"%ProgramFiles%\7-Zip\7z.exe" a -tzip %temp%/%version%-bin-res-8.zip "..\Terminaux.ResizeListener\bin\%releaseconfig%\net8.0\*"
 "%ProgramFiles%\7-Zip\7z.exe" a -tzip %temp%/%version%-demo.zip "..\Terminaux.Console\bin\%releaseconfig%\net8.0\*"
 if %errorlevel% == 0 goto :complete
 echo There was an error trying to pack binary (%errorlevel%).
