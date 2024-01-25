Aptivi/Terminaux

commit 70135cc7d2fd1ca51ed178dc3c9c8698320e72b3

imp - Direct to TWC.WP() to call the non-standalone wrap...
...per

---

We need to direct to TWC.WP() to call the non-standalone wrapper in case we need to add edge cases that apply to all calls to that function later.

---

Type: imp
Breaking: False
Doc Required: False
Part: 1/1

Terminaux/Reader/Bindings/BaseBindings/DebugPos.cs
@@ -21,6 +21,7 @@ using System;
 using System.Threading;
 using Terminaux.Base;
 using Terminaux.Reader.Tools;
+using Terminaux.Writer.ConsoleWriters;
 using Textify.General;
 
 namespace Terminaux.Reader.Bindings.BaseBindings
@@ -42,34 +43,34 @@ namespace Terminaux.Reader.Bindings.BaseBindings
             // state.InputPromptLeft, state.InputPromptTop
             Console.BackgroundColor = ConsoleColor.Green;
             ConsoleWrapper.SetCursorPosition(state.InputPromptLeft, state.InputPromptTop);
-            ConsoleWrapper.Write("I", state.settings);
+            TextWriterColor.WritePlain("I", state.settings);
             Thread.Sleep(1000);
 
             // state.CurrentCursorPosLeft, state.CurrentCursorPosTop
             Console.BackgroundColor = ConsoleColor.Red;
             PositioningTools.Commit(state);
-            ConsoleWrapper.Write("C", state.settings);
+            TextWriterColor.WritePlain("C", state.settings);
             Thread.Sleep(1000);
 
             // state.LeftMargin, state.InputPromptTop
             // Targets: state.MaximumInputPositionLeft, state.LongestSentenceLengthFromLeft
             Console.BackgroundColor = ConsoleColor.Yellow;
             ConsoleWrapper.SetCursorPosition(state.LeftMargin, state.InputPromptTop);
-            ConsoleWrapper.Write("|", state.settings);
+            TextWriterColor.WritePlain("|", state.settings);
             ConsoleWrapper.SetCursorPosition(state.MaximumInputPositionLeft, state.InputPromptTop);
-            ConsoleWrapper.Write("M", state.settings);
+            TextWriterColor.WritePlain("M", state.settings);
             ConsoleWrapper.SetCursorPosition(state.LongestSentenceLengthFromLeft >= ConsoleWrapper.WindowWidth ? state.LongestSentenceLengthFromLeft - 1 : state.LongestSentenceLengthFromLeft, state.InputPromptTop);
-            ConsoleWrapper.Write("L", state.settings);
+            TextWriterColor.WritePlain("L", state.settings);
 
             // 0, state.InputPromptTop
             // Targets: state.LongestSentenceLengthFromLeftForFirstLine, state.LongestSentenceLengthFromLeftForGeneralLine
             Console.BackgroundColor = ConsoleColor.DarkYellow;
             ConsoleWrapper.SetCursorPosition(0, state.InputPromptTop);
-            ConsoleWrapper.Write("#", state.settings);
+            TextWriterColor.WritePlain("#", state.settings);
             ConsoleWrapper.SetCursorPosition(state.LongestSentenceLengthFromLeftForFirstLine, state.InputPromptTop);
-            ConsoleWrapper.Write("F", state.settings);
+            TextWriterColor.WritePlain("F", state.settings);
             ConsoleWrapper.SetCursorPosition(state.LongestSentenceLengthFromLeftForGeneralLine, state.InputPromptTop);
-            ConsoleWrapper.Write("G", state.settings);
+            TextWriterColor.WritePlain("G", state.settings);
             Thread.Sleep(1000);
 
             // Re-write the text and set the current cursor position as appropriate
@@ -83,28 +84,28 @@ namespace Terminaux.Reader.Bindings.BaseBindings
             PositioningTools.SeekTo(renderedText.Length, ref state);
             Console.BackgroundColor = ConsoleColor.Blue;
             PositioningTools.Commit(state);
-            ConsoleWrapper.Write("S", state.settings);
+            TextWriterColor.WritePlain("S", state.settings);
             Thread.Sleep(1000);
 
             // Verify seek to 0
             PositioningTools.SeekTo(0, ref state);
             Console.BackgroundColor = ConsoleColor.DarkBlue;
             PositioningTools.Commit(state);
-            ConsoleWrapper.Write("Z", state.settings);
+            TextWriterColor.WritePlain("Z", state.settings);
             Thread.Sleep(1000);
 
             // Verify going forward 5 times
             PositioningTools.GoForward(5, ref state);
             Console.BackgroundColor = ConsoleColor.Magenta;
             PositioningTools.Commit(state);
-            ConsoleWrapper.Write("5", state.settings);
+            TextWriterColor.WritePlain("5", state.settings);
             Thread.Sleep(1000);
 
             // Verify going backward 3 times
             PositioningTools.GoBack(3, ref state);
             Console.BackgroundColor = ConsoleColor.DarkMagenta;
             PositioningTools.Commit(state);
-            ConsoleWrapper.Write("3", state.settings);
+            TextWriterColor.WritePlain("3", state.settings);
             Thread.Sleep(1000);
 
             // Now, reset everything

Terminaux/Reader/Bindings/BaseBindings/NextHistory.cs
@@ -20,6 +20,7 @@
 using System;
 using Terminaux.Base;
 using Terminaux.Reader.Tools;
+using Terminaux.Writer.ConsoleWriters;
 
 namespace Terminaux.Reader.Bindings.BaseBindings
 {
@@ -54,7 +55,7 @@ namespace Terminaux.Reader.Bindings.BaseBindings
                 int longestSentenceLength = state.LongestSentenceLengthFromLeftForFirstLine;
                 string renderedBlanks = new(' ', longestSentenceLength);
                 ConsoleWrapper.SetCursorPosition(state.InputPromptLeft, state.InputPromptTop);
-                ConsoleWrapper.Write(renderedBlanks, state.settings);
+                TextWriterColor.WritePlain(renderedBlanks, state.settings);
                 PositioningTools.SeekTo(0, ref state);
                 state.currentTextPos = 0;
                 state.currentCursorPosLeft = state.InputPromptLeft;
@@ -63,7 +64,7 @@ namespace Terminaux.Reader.Bindings.BaseBindings
             else
             {
                 ConsoleWrapper.SetCursorPosition(state.InputPromptLeft, state.InputPromptTop);
-                ConsoleWrapper.Write(new string(' ', length), state.settings);
+                TextWriterColor.WritePlain(new string(' ', length), state.settings);
                 PositioningTools.SeekTo(0, ref state);
             }
             PositioningTools.Commit(state);

Terminaux/Reader/Bindings/BaseBindings/PreviousHistory.cs
@@ -20,6 +20,7 @@
 using System;
 using Terminaux.Base;
 using Terminaux.Reader.Tools;
+using Terminaux.Writer.ConsoleWriters;
 
 namespace Terminaux.Reader.Bindings.BaseBindings
 {
@@ -54,7 +55,7 @@ namespace Terminaux.Reader.Bindings.BaseBindings
                 int longestSentenceLength = state.LongestSentenceLengthFromLeftForFirstLine;
                 string renderedBlanks = new(' ', longestSentenceLength);
                 ConsoleWrapper.SetCursorPosition(state.InputPromptLeft, state.InputPromptTop);
-                ConsoleWrapper.Write(renderedBlanks, state.settings);
+                TextWriterColor.WritePlain(renderedBlanks, state.settings);
                 PositioningTools.SeekTo(0, ref state);
                 state.currentTextPos = 0;
                 state.currentCursorPosLeft = state.InputPromptLeft;
@@ -63,7 +64,7 @@ namespace Terminaux.Reader.Bindings.BaseBindings
             else
             {
                 ConsoleWrapper.SetCursorPosition(state.InputPromptLeft, state.InputPromptTop);
-                ConsoleWrapper.Write(new string(' ', length), state.settings);
+                TextWriterColor.WritePlain(new string(' ', length), state.settings);
                 PositioningTools.SeekTo(0, ref state);
             }
             PositioningTools.Commit(state);

commit 078d273b8b49a863b6582e1d0ba539e03d7990b4

fix - reg - Fixed regression caused by 70135cc
---

We've fixed a regression caused by commit 70135cc7d2fd1ca51ed178dc3c9c8698320e72b3 that caused Terminaux's Reader to make a new line when advancing through the history using either UP ARROW or DOWN ARROW keys.

---

Type: fix
Breaking: False
Doc Required: False
Part: 1/1

Terminaux/Reader/Bindings/BaseBindings/DebugPos.cs
@@ -43,34 +43,34 @@ namespace Terminaux.Reader.Bindings.BaseBindings
             // state.InputPromptLeft, state.InputPromptTop
             Console.BackgroundColor = ConsoleColor.Green;
             ConsoleWrapper.SetCursorPosition(state.InputPromptLeft, state.InputPromptTop);
-            TextWriterColor.WritePlain("I", state.settings);
+            TextWriterColor.WritePlain("I", false, state.settings);
             Thread.Sleep(1000);
 
             // state.CurrentCursorPosLeft, state.CurrentCursorPosTop
             Console.BackgroundColor = ConsoleColor.Red;
             PositioningTools.Commit(state);
-            TextWriterColor.WritePlain("C", state.settings);
+            TextWriterColor.WritePlain("C", false, state.settings);
             Thread.Sleep(1000);
 
             // state.LeftMargin, state.InputPromptTop
             // Targets: state.MaximumInputPositionLeft, state.LongestSentenceLengthFromLeft
             Console.BackgroundColor = ConsoleColor.Yellow;
             ConsoleWrapper.SetCursorPosition(state.LeftMargin, state.InputPromptTop);
-            TextWriterColor.WritePlain("|", state.settings);
+            TextWriterColor.WritePlain("|", false, state.settings);
             ConsoleWrapper.SetCursorPosition(state.MaximumInputPositionLeft, state.InputPromptTop);
-            TextWriterColor.WritePlain("M", state.settings);
+            TextWriterColor.WritePlain("M", false, state.settings);
             ConsoleWrapper.SetCursorPosition(state.LongestSentenceLengthFromLeft >= ConsoleWrapper.WindowWidth ? state.LongestSentenceLengthFromLeft - 1 : state.LongestSentenceLengthFromLeft, state.InputPromptTop);
-            TextWriterColor.WritePlain("L", state.settings);
+            TextWriterColor.WritePlain("L", false, state.settings);
 
             // 0, state.InputPromptTop
             // Targets: state.LongestSentenceLengthFromLeftForFirstLine, state.LongestSentenceLengthFromLeftForGeneralLine
             Console.BackgroundColor = ConsoleColor.DarkYellow;
             ConsoleWrapper.SetCursorPosition(0, state.InputPromptTop);
-            TextWriterColor.WritePlain("#", state.settings);
+            TextWriterColor.WritePlain("#", false, state.settings);
             ConsoleWrapper.SetCursorPosition(state.LongestSentenceLengthFromLeftForFirstLine, state.InputPromptTop);
-            TextWriterColor.WritePlain("F", state.settings);
+            TextWriterColor.WritePlain("F", false, state.settings);
             ConsoleWrapper.SetCursorPosition(state.LongestSentenceLengthFromLeftForGeneralLine, state.InputPromptTop);
-            TextWriterColor.WritePlain("G", state.settings);
+            TextWriterColor.WritePlain("G", false, state.settings);
             Thread.Sleep(1000);
 
             // Re-write the text and set the current cursor position as appropriate
@@ -84,28 +84,28 @@ namespace Terminaux.Reader.Bindings.BaseBindings
             PositioningTools.SeekTo(renderedText.Length, ref state);
             Console.BackgroundColor = ConsoleColor.Blue;
             PositioningTools.Commit(state);
-            TextWriterColor.WritePlain("S", state.settings);
+            TextWriterColor.WritePlain("S", false, state.settings);
             Thread.Sleep(1000);
 
             // Verify seek to 0
             PositioningTools.SeekTo(0, ref state);
             Console.BackgroundColor = ConsoleColor.DarkBlue;
             PositioningTools.Commit(state);
-            TextWriterColor.WritePlain("Z", state.settings);
+            TextWriterColor.WritePlain("Z", false, state.settings);
             Thread.Sleep(1000);
 
             // Verify going forward 5 times
             PositioningTools.GoForward(5, ref state);
             Console.BackgroundColor = ConsoleColor.Magenta;
             PositioningTools.Commit(state);
-            TextWriterColor.WritePlain("5", state.settings);
+            TextWriterColor.WritePlain("5", false, state.settings);
             Thread.Sleep(1000);
 
             // Verify going backward 3 times
             PositioningTools.GoBack(3, ref state);
             Console.BackgroundColor = ConsoleColor.DarkMagenta;
             PositioningTools.Commit(state);
-            TextWriterColor.WritePlain("3", state.settings);
+            TextWriterColor.WritePlain("3", false, state.settings);
             Thread.Sleep(1000);
 
             // Now, reset everything

Terminaux/Reader/Bindings/BaseBindings/NextHistory.cs
@@ -55,7 +55,7 @@ namespace Terminaux.Reader.Bindings.BaseBindings
                 int longestSentenceLength = state.LongestSentenceLengthFromLeftForFirstLine;
                 string renderedBlanks = new(' ', longestSentenceLength);
                 ConsoleWrapper.SetCursorPosition(state.InputPromptLeft, state.InputPromptTop);
-                TextWriterColor.WritePlain(renderedBlanks, state.settings);
+                TextWriterColor.WritePlain(renderedBlanks, false, state.settings);
                 PositioningTools.SeekTo(0, ref state);
                 state.currentTextPos = 0;
                 state.currentCursorPosLeft = state.InputPromptLeft;
@@ -64,7 +64,7 @@ namespace Terminaux.Reader.Bindings.BaseBindings
             else
             {
                 ConsoleWrapper.SetCursorPosition(state.InputPromptLeft, state.InputPromptTop);
-                TextWriterColor.WritePlain(new string(' ', length), state.settings);
+                TextWriterColor.WritePlain(new string(' ', length), false, state.settings);
                 PositioningTools.SeekTo(0, ref state);
             }
             PositioningTools.Commit(state);

Terminaux/Reader/Bindings/BaseBindings/PreviousHistory.cs
@@ -55,7 +55,7 @@ namespace Terminaux.Reader.Bindings.BaseBindings
                 int longestSentenceLength = state.LongestSentenceLengthFromLeftForFirstLine;
                 string renderedBlanks = new(' ', longestSentenceLength);
                 ConsoleWrapper.SetCursorPosition(state.InputPromptLeft, state.InputPromptTop);
-                TextWriterColor.WritePlain(renderedBlanks, state.settings);
+                TextWriterColor.WritePlain(renderedBlanks, false, state.settings);
                 PositioningTools.SeekTo(0, ref state);
                 state.currentTextPos = 0;
                 state.currentCursorPosLeft = state.InputPromptLeft;
@@ -64,7 +64,7 @@ namespace Terminaux.Reader.Bindings.BaseBindings
             else
             {
                 ConsoleWrapper.SetCursorPosition(state.InputPromptLeft, state.InputPromptTop);
-                TextWriterColor.WritePlain(new string(' ', length), state.settings);
+                TextWriterColor.WritePlain(new string(' ', length), false, state.settings);
                 PositioningTools.SeekTo(0, ref state);
             }
             PositioningTools.Commit(state);