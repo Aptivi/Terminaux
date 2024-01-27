84998a9fdc2d5acf2768862df5bc315aaabe2e80
Aptivi CEO <eoflaoevicecity@gmail.com>
2024-01-27 20:32:53 +0300

    imp - Make truncated text appear consistently
    ---
    
    We need to make the truncated text appear consistently in small boxes with a title.
    
    ---
    
    Type: imp
    Breaking: False
    Doc Required: False
    Part: 1/1

--- Terminaux/Writer/FancyWriters/BoxFrameColor.cs
+++ Terminaux/Writer/FancyWriters/BoxFrameColor.cs
@@ -690,9 +690,9 @@ namespace Terminaux.Writer.FancyWriters
                 );
 
                 // Text title
-                if (!string.IsNullOrEmpty(text) && InteriorWidth - 6 > 0)
+                if (!string.IsNullOrEmpty(text) && InteriorWidth - 7 > 0)
                 {
-                    string finalText = $" {TextTools.FormatString(text, vars).Truncate(InteriorWidth - 6)} ";
+                    string finalText = $" {TextTools.FormatString(text, vars).Truncate(InteriorWidth - 7)} ";
                     frameBuilder.Append(
                         $"{CsiSequences.GenerateCsiCursorPosition(Left + 3, Top + 1)}" +
                         $"{finalText}"
