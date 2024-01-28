d936fdaf5b805c69e892538aff80e2821aa1d25f
Aptivi CEO <eoflaoevicecity@gmail.com>
2024-01-28 13:42:26 +0300

    add - Added huge listing facades
    ---
    
    We've added huge listing facades.
    
    ---
    
    Type: add
    Breaking: False
    Doc Required: False
    Part: 1/1

---
+++ Terminaux.Console/Fixtures/Cases/TestListWriterHuge.cs
@@ -0,0 +1,46 @@
+﻿//
+// Terminaux  Copyright (C) 2023-2024  Aptivi
+//
+// This file is part of Terminaux
+//
+// Terminaux is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Terminaux is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY, without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+
+using System.Collections.Generic;
+using Terminaux.Colors.Data;
+using Terminaux.Writer.ConsoleWriters;
+using Textify.NameGen;
+
+namespace Terminaux.ConsoleDemo.Fixtures.Cases
+{
+    internal class TestListWriterHuge : IFixture
+    {
+        public string FixtureID => "TestListWriterHuge";
+        public void RunFixture()
+        {
+            var choices = new List<string>();
+            var arrayChoices = new List<string[]>();
+            var names = NameGenerator.FindFirstNames("");
+            for (int i = 0; i < names.Length; i++)
+            {
+                choices.Add(names[i]);
+                arrayChoices.Add([$"Number #{i + 1}", names[i]]);
+            }
+            TextWriterColor.Write("Normal string list:");
+            ListWriterColor.WriteList(choices, ConsoleColors.Gray, ConsoleColors.DarkGray, false);
+            TextWriterColor.Write("Array string list:");
+            ListWriterColor.WriteList(arrayChoices, ConsoleColors.Gray, ConsoleColors.DarkGray, false);
+        }
+    }
+}
