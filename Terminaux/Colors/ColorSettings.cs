d0c0bee80cb1ca7a93aa8e0bf49bdd48c27a2bcb
Aptivi CEO <eoflaoevicecity@gmail.com>
2024-01-28 16:02:34 +0300

    add - ptp|prt|doc - Initial implementation of "fake Alpha" for consoles
    ---
    
    We've added an initial implementation of the "fake Alpha" feature that allows you to set the "transparency" of the colors in the terminal.
    
    The next part will cover the whole implementation and putting it to the test as a separate test facade.
    
    This doesn't mean that any text or element behind "transparent" boxes or elements will show up inside the console, because Terminaux isn't designed for use in GUI apps except for very specific circumstances.
    
    ---
    
    Type: add
    Breaking: False
    Doc Required: True
    Part: 1/2

--- Terminaux/Colors/ColorSettings.cs
+++ Terminaux/Colors/ColorSettings.cs
@@ -28,6 +28,8 @@ namespace Terminaux.Colors
     public class ColorSettings
     {
         private double _blindnessSeverity = 0.6;
+        private int _alpha = 255;
+        private Color _opacityColor = null;
 
         /// <summary>
         /// Enables the color transformation to adjust to color blindness upon making a new instance of color
@@ -66,6 +68,50 @@ namespace Terminaux.Colors
 
                 _blindnessSeverity = value;
             }
+        }
+
+        /// <summary>
+        /// The opacity at which the color will be calculated.
+        /// </summary>
+        /// <remarks>
+        /// This fades the color to the current background color of the console currently set by
+        /// <see cref="ColorTools.SetConsoleColor(Color, bool, bool, bool)"/>. That color can be overridable by
+        /// setting the <see cref="OpacityColor"/> property to any color you want. Set it to 0, and you'll get the
+        /// color that is the same as the value of the <see cref="OpacityColor"/> property. Set it to 255, and you'll
+        /// get the color that you've created using the <see cref="Color(int, int, int, ColorSettings)"/> constructor
+        /// and its siblings.
+        /// <br></br>
+        /// <br></br>
+        /// This is still fake transparency for GUI applications, but should behave like real transparency in console
+        /// applications that use background colors.
+        /// </remarks>
+        public int Opacity
+        {
+            get => _alpha;
+            set
+            {
+                // Check the ranges
+                if (value < 0)
+                    value = 0;
+                if (value > 255)
+                    value = 255;
+
+                // Now, set it!
+                _alpha = value;
+            }
+        }
+
+        /// <summary>
+        /// The opacity color to fade the new instances of colors to using the opacity as the threshold.
+        /// </summary>
+        /// <remarks>
+        /// See the Remarks section of <see cref="Opacity"/> for more info about how we use this color to calculate
+        /// the transparency.
+        /// </remarks>
+        public Color OpacityColor
+        {
+            get => _opacityColor ?? ColorTools.CurrentBackgroundColor;
+            set => _opacityColor = value ?? ColorTools.CurrentBackgroundColor;
         }
     }
 }
