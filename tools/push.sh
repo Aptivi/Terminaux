e2ee201fb35508db6728ea18b40685179dd1c640
Aptivi <eoflaoevicecity@gmail.com>
2024-04-22 12:19:01 +0300

    upd - Added the push scripts and used them

--- tools/push.sh
+++ tools/push.sh
@@ -1,22 +1,4 @@
 #!/bin/bash
-
-#   Terminaux  Copyright (C) 2023  Aptivi
-# 
-#   This file is part of Terminaux
-# 
-#   Terminaux is free software: you can redistribute it and/or modify
-#   it under the terms of the GNU General Public License as published by
-#   the Free Software Foundation, either version 3 of the License, or
-#   (at your option) any later version.
-# 
-#   Terminaux is distributed in the hope that it will be useful,
-#   but WITHOUT ANY WARRANTY; without even the implied warranty of
-#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#   GNU General Public License for more details.
-# 
-#   You should have received a copy of the GNU General Public License
-#   along with this program.  If not, see <https://www.gnu.org/licenses/>.
-
 # This script pushes. Use when you have dotnet installed.
 releaseconf=$1
 if [ -z $releaseconf ]; then
