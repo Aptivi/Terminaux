af4f33c7415c10b0f18ff4bd68e4031ae7ff5599
Aptivi CEO <eoflaoevicecity@gmail.com>
2024-05-18 19:11:45 +0300

    add - doc - Added nerd font tools (pt. 1)
    ---
    
    We've added Nerd Fonts tools. Please note that the IDE is for some reason not detecting the NerdFontsTypes type, but MSBuild works fine regardless.
    
    Extending the implementation soon...
    
    ---
    
    Type: add
    Breaking: False
    Doc Required: True
    Part: 1/1

---
+++ Terminaux.NerdFontGen/Decoy/NerdFontInfo.cs
@@ -0,0 +1,43 @@
+﻿//
+// Terminaux  Copyright (C) 2023-2024  Aptivi
+//
+// This file is part of Terminaux
+//
+// Terminaux is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Terminaux is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY, without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+
+using Newtonsoft.Json;
+
+namespace Terminaux.NerdFontGen.Decoy
+{
+    public class Icon
+    {
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("unicode")]
+        public string Unicode { get; set; }
+    }
+
+    public class NerdFontInfo
+    {
+        [JsonProperty("type")]
+        public string Type { get; set; }
+
+        [JsonProperty("icons")]
+        public Icon[] Icons { get; set; }
+    }
+
+
+}
