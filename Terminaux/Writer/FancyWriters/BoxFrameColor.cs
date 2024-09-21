e241c0d654e752c52b72413ae1964d50faa7322b
Aptivi <eoflaoevicecity@gmail.com>
2024-09-21 13:26:17 +0300

    imp - Made titles in box frames more awesome
    ---
    
    The titles in the box frames are now surrounded by the horizontal intersection characters that can be configured.
    
    ---
    
    Type: imp
    Breaking: False
    Doc Required: False
    Backport Required: False
    Part: 1/1

--- Terminaux/Writer/FancyWriters/BoxFrameColor.cs
+++ Terminaux/Writer/FancyWriters/BoxFrameColor.cs
@@ -625,9 +625,9 @@ namespace Terminaux.Writer.FancyWriters
                 }
 
                 // Text title
-                if (!string.IsNullOrEmpty(text) && InteriorWidth - 6 > 0)
+                if (!string.IsNullOrEmpty(text) && InteriorWidth - 8 > 0)
                 {
-                    string finalText = $" {TextTools.FormatString(text, vars).Truncate(InteriorWidth - 6)} ";
+                    string finalText = $"{settings.BorderRightHorizontalIntersectionChar} {TextTools.FormatString(text, vars).Truncate(InteriorWidth - 8)} {settings.BorderLeftHorizontalIntersectionChar}";
                     frameBuilder.Append(
                         $"{CsiSequences.GenerateCsiCursorPosition(Left + 3, Top + 1)}" +
                         $"{finalText}"
