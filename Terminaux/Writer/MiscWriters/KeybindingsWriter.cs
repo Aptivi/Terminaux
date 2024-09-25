317c0f1ebeae41e0825ce07e1ff54e0741d0d96f
Aptivi <eoflaoevicecity@gmail.com>
2024-09-25 10:15:46 +0300

    add - Added appropriate overloads for ShowKeybindi...
    ...ngInfobox()
    
    ---
    
    We've added overloads for the ShowKeybindingInfobox() function that are similar to the infobox writer function overloads.
    
    ---
    
    Type: add
    Breaking: False
    Doc Required: False
    Backport Required: False
    Part: 1/1

--- Terminaux/Writer/MiscWriters/KeybindingsWriter.cs
+++ Terminaux/Writer/MiscWriters/KeybindingsWriter.cs
@@ -27,6 +27,7 @@ using Terminaux.Inputs.Styles.Infobox;
 using Terminaux.Sequences;
 using Terminaux.Sequences.Builder.Types;
 using Terminaux.Writer.ConsoleWriters;
+using Terminaux.Writer.FancyWriters.Tools;
 using Terminaux.Writer.MiscWriters.Tools;
 
 namespace Terminaux.Writer.MiscWriters
@@ -323,13 +324,175 @@ namespace Terminaux.Writer.MiscWriters
         }
 
         /// <summary>
-        /// Show the keybinding help informational box
+        /// Writes the info box plainly
         /// </summary>
         /// <param name="keybindings">Keybindings (including the built-in ones)</param>
-        public static void ShowKeybindingInfobox(Keybinding[] keybindings)
+        /// <param name="vars">Variables to format the message before it's written.</param>
+        public static void ShowKeybindingInfoboxPlain(Keybinding[] keybindings, params object[] vars) =>
+            ShowKeybindingInfoboxPlain(keybindings, BorderSettings.GlobalSettings, true, vars);
+
+        /// <summary>
+        /// Writes the info box plainly
+        /// </summary>
+        /// <param name="settings">Border settings to use</param>
+        /// <param name="keybindings">Keybindings (including the built-in ones)</param>
+        /// <param name="vars">Variables to format the message before it's written.</param>
+        public static void ShowKeybindingInfoboxPlain(Keybinding[] keybindings, BorderSettings settings, params object[] vars) =>
+            ShowKeybindingInfoboxPlain("Available keybindings", keybindings, settings, vars);
+
+        /// <summary>
+        /// Writes the info box plainly
+        /// </summary>
+        /// <param name="keybindings">Keybindings (including the built-in ones)</param>
+        /// <param name="vars">Variables to format the message before it's written.</param>
+        public static void ShowKeybindingInfobox(Keybinding[] keybindings, params object[] vars) =>
+            ShowKeybindingInfoboxColorBack(keybindings, BorderSettings.GlobalSettings, new Color(ConsoleColors.Silver), ColorTools.currentBackgroundColor, vars);
+
+        /// <summary>
+        /// Writes the info box plainly
+        /// </summary>
+        /// <param name="InfoBoxColor">InfoBox color</param>
+        /// <param name="keybindings">Keybindings (including the built-in ones)</param>
+        /// <param name="vars">Variables to format the message before it's written.</param>
+        public static void ShowKeybindingInfoboxColor(Keybinding[] keybindings, Color InfoBoxColor, params object[] vars) =>
+            ShowKeybindingInfoboxColorBack(keybindings, BorderSettings.GlobalSettings, InfoBoxColor, ColorTools.currentBackgroundColor, vars);
+
+        /// <summary>
+        /// Writes the info box plainly
+        /// </summary>
+        /// <param name="InfoBoxColor">InfoBox color</param>
+        /// <param name="BackgroundColor">InfoBox background color</param>
+        /// <param name="keybindings">Keybindings (including the built-in ones)</param>
+        /// <param name="vars">Variables to format the message before it's written.</param>
+        public static void ShowKeybindingInfoboxColorBack(Keybinding[] keybindings, Color InfoBoxColor, Color BackgroundColor, params object[] vars) =>
+            ShowKeybindingInfoboxColorBack(keybindings, BorderSettings.GlobalSettings, InfoBoxColor, BackgroundColor, vars);
+
+        /// <summary>
+        /// Writes the info box plainly
+        /// </summary>
+        /// <param name="settings">Border settings to use</param>
+        /// <param name="keybindings">Keybindings (including the built-in ones)</param>
+        /// <param name="vars">Variables to format the message before it's written.</param>
+        public static void ShowKeybindingInfobox(Keybinding[] keybindings, BorderSettings settings, params object[] vars) =>
+            ShowKeybindingInfoboxColorBack(keybindings, settings, new Color(ConsoleColors.Silver), ColorTools.currentBackgroundColor, vars);
+
+        /// <summary>
+        /// Writes the info box plainly
+        /// </summary>
+        /// <param name="settings">Border settings to use</param>
+        /// <param name="InfoBoxColor">InfoBox color</param>
+        /// <param name="keybindings">Keybindings (including the built-in ones)</param>
+        /// <param name="vars">Variables to format the message before it's written.</param>
+        public static void ShowKeybindingInfoboxColor(Keybinding[] keybindings, BorderSettings settings, Color InfoBoxColor, params object[] vars) =>
+            ShowKeybindingInfoboxColorBack(keybindings, settings, InfoBoxColor, ColorTools.currentBackgroundColor, vars);
+
+        /// <summary>
+        /// Writes the info box plainly
+        /// </summary>
+        /// <param name="settings">Border settings to use</param>
+        /// <param name="InfoBoxColor">InfoBox color</param>
+        /// <param name="BackgroundColor">InfoBox background color</param>
+        /// <param name="keybindings">Keybindings (including the built-in ones)</param>
+        /// <param name="vars">Variables to format the message before it's written.</param>
+        public static void ShowKeybindingInfoboxColorBack(Keybinding[] keybindings, BorderSettings settings, Color InfoBoxColor, Color BackgroundColor, params object[] vars) =>
+            ShowKeybindingInfoboxColorBack("Available keybindings", keybindings, settings, InfoBoxColor, BackgroundColor, vars);
+
+        /// <summary>
+        /// Writes the info box plainly
+        /// </summary>
+        /// <param name="title">Title to be written</param>
+        /// <param name="keybindings">Keybindings (including the built-in ones)</param>
+        /// <param name="vars">Variables to format the message before it's written.</param>
+        public static void ShowKeybindingInfoboxPlain(string title, Keybinding[] keybindings, params object[] vars) =>
+            ShowKeybindingInfoboxPlain(title, keybindings, BorderSettings.GlobalSettings, vars);
+
+        /// <summary>
+        /// Writes the info box plainly
+        /// </summary>
+        /// <param name="settings">Border settings to use</param>
+        /// <param name="title">Title to be written</param>
+        /// <param name="keybindings">Keybindings (including the built-in ones)</param>
+        /// <param name="vars">Variables to format the message before it's written.</param>
+        public static void ShowKeybindingInfoboxPlain(string title, Keybinding[] keybindings, BorderSettings settings, params object[] vars) =>
+            ShowKeybindingInfoboxColorBack(title, keybindings, settings, new Color(ConsoleColors.Silver), ColorTools.currentBackgroundColor, false, vars);
+
+        /// <summary>
+        /// Writes the info box plainly
+        /// </summary>
+        /// <param name="title">Title to be written</param>
+        /// <param name="keybindings">Keybindings (including the built-in ones)</param>
+        /// <param name="vars">Variables to format the message before it's written.</param>
+        public static void ShowKeybindingInfobox(string title, Keybinding[] keybindings, params object[] vars) =>
+            ShowKeybindingInfoboxColorBack(title, keybindings, BorderSettings.GlobalSettings, new Color(ConsoleColors.Silver), ColorTools.currentBackgroundColor, vars);
+
+        /// <summary>
+        /// Writes the info box plainly
+        /// </summary>
+        /// <param name="title">Title to be written</param>
+        /// <param name="InfoBoxTitledColor">InfoBoxTitled color</param>
+        /// <param name="keybindings">Keybindings (including the built-in ones)</param>
+        /// <param name="vars">Variables to format the message before it's written.</param>
+        public static void ShowKeybindingInfoboxColor(string title, Keybinding[] keybindings, Color InfoBoxTitledColor, params object[] vars) =>
+            ShowKeybindingInfoboxColorBack(title, keybindings, BorderSettings.GlobalSettings, InfoBoxTitledColor, ColorTools.currentBackgroundColor, vars);
+
+        /// <summary>
+        /// Writes the info box plainly
+        /// </summary>
+        /// <param name="title">Title to be written</param>
+        /// <param name="InfoBoxTitledColor">InfoBoxTitled color</param>
+        /// <param name="BackgroundColor">InfoBoxTitled background color</param>
+        /// <param name="keybindings">Keybindings (including the built-in ones)</param>
+        /// <param name="vars">Variables to format the message before it's written.</param>
+        public static void ShowKeybindingInfoboxColorBack(string title, Keybinding[] keybindings, Color InfoBoxTitledColor, Color BackgroundColor, params object[] vars) =>
+            ShowKeybindingInfoboxColorBack(title, keybindings, BorderSettings.GlobalSettings, InfoBoxTitledColor, BackgroundColor, vars);
+
+        /// <summary>
+        /// Writes the info box plainly
+        /// </summary>
+        /// <param name="title">Title to be written</param>
+        /// <param name="settings">Border settings to use</param>
+        /// <param name="keybindings">Keybindings (including the built-in ones)</param>
+        /// <param name="vars">Variables to format the message before it's written.</param>
+        public static void ShowKeybindingInfobox(string title, Keybinding[] keybindings, BorderSettings settings, params object[] vars) =>
+            ShowKeybindingInfoboxColorBack(title, keybindings, settings, new Color(ConsoleColors.Silver), ColorTools.currentBackgroundColor, vars);
+
+        /// <summary>
+        /// Writes the info box plainly
+        /// </summary>
+        /// <param name="title">Title to be written</param>
+        /// <param name="settings">Border settings to use</param>
+        /// <param name="InfoBoxTitledColor">InfoBoxTitled color</param>
+        /// <param name="keybindings">Keybindings (including the built-in ones)</param>
+        /// <param name="vars">Variables to format the message before it's written.</param>
+        public static void ShowKeybindingInfoboxColor(string title, Keybinding[] keybindings, BorderSettings settings, Color InfoBoxTitledColor, params object[] vars) =>
+            ShowKeybindingInfoboxColorBack(title, keybindings, settings, InfoBoxTitledColor, ColorTools.currentBackgroundColor, vars);
+
+        /// <summary>
+        /// Writes the info box plainly
+        /// </summary>
+        /// <param name="title">Title to be written</param>
+        /// <param name="settings">Border settings to use</param>
+        /// <param name="InfoBoxTitledColor">InfoBoxTitled color</param>
+        /// <param name="BackgroundColor">InfoBoxTitled background color</param>
+        /// <param name="keybindings">Keybindings (including the built-in ones)</param>
+        /// <param name="vars">Variables to format the message before it's written.</param>
+        public static void ShowKeybindingInfoboxColorBack(string title, Keybinding[] keybindings, BorderSettings settings, Color InfoBoxTitledColor, Color BackgroundColor, params object[] vars) =>
+            ShowKeybindingInfoboxColorBack(title, keybindings, settings, InfoBoxTitledColor, BackgroundColor, true, vars);
+
+        /// <summary>
+        /// Writes the info box plainly
+        /// </summary>
+        /// <param name="title">Title to be written</param>
+        /// <param name="settings">Border settings to use</param>
+        /// <param name="InfoBoxTitledColor">InfoBoxTitled color</param>
+        /// <param name="BackgroundColor">InfoBoxTitled background color</param>
+        /// <param name="keybindings">Keybindings (including the built-in ones)</param>
+        /// <param name="useColor">Whether to use color or not</param>
+        /// <param name="vars">Variables to format the message before it's written.</param>
+        internal static void ShowKeybindingInfoboxColorBack(string title, Keybinding[] keybindings, BorderSettings settings, Color InfoBoxTitledColor, Color BackgroundColor, bool useColor, params object[] vars)
         {
             string keybindingsText = RenderKeybindingHelpText(keybindings);
-            InfoBoxColor.WriteInfoBox("Available keybindings", keybindingsText);
+            InfoBoxColor.WriteInfoBoxColorBack(title, keybindingsText, true, settings, InfoBoxTitledColor, BackgroundColor, useColor, vars);
         }
 
         /// <summary>
