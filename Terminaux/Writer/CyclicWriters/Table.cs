f57d91c9ac86dc52a29a1d80fab7abbe851b6204
Aptivi <eoflaoevicecity@gmail.com>
2024-11-03 11:50:38 +0300

    ref - Refactored TableColor
    ---
    
    Starting from this version of Terminaux, the mentioned class is obsolete
    and its equivalent cyclic writer must be used. This is to reduce
    complexity associated with maintaining the old-school methods in favor of
    the cyclic writer equivalent.
    
    Deprecations will be resolved later to make sure that the codebase is free
    of warnings.
    
    ---
    
    Type: ref
    Breaking: False
    Doc Required: False
    Backport Required: False
    Part: 1/1

--- Terminaux/Writer/CyclicWriters/Table.cs
+++ Terminaux/Writer/CyclicWriters/Table.cs
@@ -18,9 +18,15 @@
 //
 
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Terminaux.Base.Extensions;
 using Terminaux.Colors;
+using Terminaux.Sequences.Builder.Types;
+using Terminaux.Writer.ConsoleWriters;
 using Terminaux.Writer.FancyWriters;
 using Terminaux.Writer.FancyWriters.Tools;
+using Terminaux.Writer.MiscWriters.Tools;
 using Textify.General;
 
 namespace Terminaux.Writer.CyclicWriters
@@ -178,8 +184,144 @@ namespace Terminaux.Writer.CyclicWriters
         /// <returns>Rendered text that will be used by the renderer</returns>
         public string Render()
         {
-            return TableColor.RenderTable(
+            return RenderTable(
                 Rows, Left, Top, InteriorWidth, InteriorHeight, Header, SeparatorColor, HeaderColor, ValueColor, BackgroundColor, customColor, Settings, BorderSettings);
+        }
+
+        internal static string RenderTable(string[,] Rows, int left, int top, int width, int height, bool enableHeader, Color SeparatorForegroundColor, Color HeaderForegroundColor, Color ValueForegroundColor, Color BackgroundColor, bool useColor, List<CellOptions>? CellOptions = null, BorderSettings? tableBorderSettings = null)
+        {
+            // Create a border which the table will be drawn on
+            var tableBuilder = new StringBuilder();
+            tableBorderSettings ??= new();
+
+            // Determine the positions
+            int columnsCount = Rows.GetLength(1);
+            int rowsCount = Rows.GetLength(0);
+            (int, int)[,] positions = new (int, int)[columnsCount, rowsCount];
+            int maxCellWidth = (width + 2) / columnsCount;
+            for (int x = 0; x < columnsCount; x++)
+            {
+                for (int y = 0; y < rowsCount; y++)
+                {
+                    int finalPosX = left + maxCellWidth * x + 1;
+                    int finalPosY = top + y + 1;
+                    if (enableHeader && y > 0)
+                        finalPosY++;
+                    positions[x, y] = (finalPosX, finalPosY);
+                }
+            }
+
+            // Use the final width to create the actual table
+            if (useColor)
+            {
+                tableBuilder.Append(
+                    BorderColor.RenderBorder(left, top, maxCellWidth * columnsCount - 1, height, tableBorderSettings, SeparatorForegroundColor, BackgroundColor) +
+                    ColorTools.RenderSetConsoleColor(SeparatorForegroundColor) +
+                    ColorTools.RenderSetConsoleColor(BackgroundColor, true)
+                );
+            }
+            else
+            {
+                tableBuilder.Append(
+                    BorderColor.RenderBorderPlain(left, top, maxCellWidth * columnsCount - 1, height, tableBorderSettings)
+                );
+            }
+
+            // Create a header separator if we need a header
+            if (enableHeader)
+            {
+                char begin = tableBorderSettings.BorderLeftHorizontalIntersectionChar;
+                char middle = tableBorderSettings.BorderHorizontalIntersectionChar;
+                char end = tableBorderSettings.BorderRightHorizontalIntersectionChar;
+                int headerBorderPosX = left;
+                int headerBorderPosY = top + 2;
+                tableBuilder.Append(
+                    CsiSequences.GenerateCsiCursorPosition(headerBorderPosX + 1, headerBorderPosY + 1) +
+                    begin + new string(middle, maxCellWidth * columnsCount - 1) + end
+                );
+            }
+
+            // Create a row separator
+            char beginVertical = tableBorderSettings.BorderTopVerticalIntersectionChar;
+            char middleVertical = tableBorderSettings.BorderVerticalIntersectionChar;
+            char endVertical = tableBorderSettings.BorderBottomVerticalIntersectionChar;
+            char intersect = tableBorderSettings.BorderWholeIntersectionChar;
+            for (int x = 1; x < columnsCount; x++)
+            {
+                // Try to get the positions for the separator
+                var positionsSeparator = ((int, int))positions.GetValue(x, 0);
+
+                // Build the separator
+                for (int y = 0; y < height + 2; y++)
+                {
+                    char finalChar =
+                        y == 0 ? beginVertical :
+                        y == height + 1 ? endVertical :
+                        y == 2 && enableHeader ? intersect :
+                        middleVertical;
+                    tableBuilder.Append(
+                        CsiSequences.GenerateCsiCursorPosition(positionsSeparator.Item1, y + top + 1) +
+                        finalChar
+                    );
+                }
+            }
+
+            // Write values
+            for (int y = 0; y < rowsCount; y++)
+            {
+                for (int x = 0; x < columnsCount; x++)
+                {
+                    // Get the initial values
+                    var positionsValues = ((int, int))positions.GetValue(x, y);
+                    if (positionsValues.Item2 > height + top)
+                        break;
+                    string text = (string)Rows.GetValue(y, x);
+                    Color finalColor =
+                        y == 0 && enableHeader ? HeaderForegroundColor :
+                        ValueForegroundColor;
+                    Color finalBackgroundColor = BackgroundColor;
+
+                    // Process them according to both the cell width and the cell options
+                    string spaces = new(' ', maxCellWidth - 1);
+                    text = text.Truncate(maxCellWidth - 4);
+                    var alignment = TextAlignment.Left;
+                    if (CellOptions is not null && CellOptions.Count > 0)
+                    {
+                        var options = CellOptions.FirstOrDefault((co) => co.ColumnIndex == x && co.RowIndex == y);
+                        if (options is not null && options.ColoredCell)
+                        {
+                            if (options.ColoredCell)
+                            {
+                                finalColor = options.CellColor;
+                                finalBackgroundColor = options.CellBackgroundColor;
+                            }
+                            alignment = options.TextSettings.Alignment;
+                        }
+                    }
+
+                    // Adjust the X position according to the alignment
+                    int alignmentPosX = TextWriterTools.DetermineTextAlignment(text, maxCellWidth - 1, alignment, positionsValues.Item1);
+
+                    // Write them
+                    if (useColor)
+                    {
+                        tableBuilder.Append(
+                            TextWriterWhereColor.RenderWhereColorBack(spaces, positionsValues.Item1, positionsValues.Item2, finalColor, finalBackgroundColor) +
+                            TextWriterWhereColor.RenderWhereColorBack(text, alignmentPosX, positionsValues.Item2, finalColor, finalBackgroundColor)
+                        );
+                    }
+                    else
+                    {
+                        tableBuilder.Append(
+                            TextWriterWhereColor.RenderWhere(spaces, positionsValues.Item1, positionsValues.Item2) +
+                            TextWriterWhereColor.RenderWhere(text, alignmentPosX, positionsValues.Item2)
+                        );
+                    }
+                }
+            }
+
+            // Final results are here
+            return tableBuilder.ToString();
         }
 
         /// <summary>
--- Terminaux/Writer/FancyWriters/TableColor.cs
+++ Terminaux/Writer/FancyWriters/TableColor.cs
@@ -28,6 +28,7 @@ using Terminaux.Colors;
 using Terminaux.Colors.Data;
 using Terminaux.Sequences.Builder.Types;
 using Terminaux.Writer.ConsoleWriters;
+using Terminaux.Writer.CyclicWriters;
 using Terminaux.Writer.FancyWriters.Tools;
 using Terminaux.Writer.MiscWriters.Tools;
 
@@ -36,6 +37,7 @@ namespace Terminaux.Writer.FancyWriters
     /// <summary>
     /// Table writer with color support
     /// </summary>
+    [Obsolete("This is considered a legacy method of writing this fancy text and will be removed in a future version of Terminaux. Please use its cyclic writer equivalent.")]
     public static class TableColor
     {
         /// <summary>
@@ -130,7 +132,7 @@ namespace Terminaux.Writer.FancyWriters
         /// <param name="CellOptions">Specifies the cell options</param>
         /// <param name="tableBorderSettings">Specifies the table border settings</param>
         public static string RenderTablePlain(string[,] Rows, int left, int top, int width, int height, bool enableHeader, List<CellOptions>? CellOptions = null, BorderSettings? tableBorderSettings = null) =>
-            RenderTable(Rows, left, top, width, height, enableHeader, ColorTools.GetGray(), ConsoleColors.Yellow, ColorTools.GetGray(), ColorTools.currentBackgroundColor, true, CellOptions, tableBorderSettings);
+            Table.RenderTable(Rows, left, top, width, height, enableHeader, ColorTools.GetGray(), ConsoleColors.Yellow, ColorTools.GetGray(), ColorTools.currentBackgroundColor, true, CellOptions, tableBorderSettings);
 
         /// <summary>
         /// Renders a table with text
@@ -148,159 +150,7 @@ namespace Terminaux.Writer.FancyWriters
         /// <param name="BackgroundColor">A background color that will be changed to.</param>
         /// <param name="tableBorderSettings">Specifies the table border settings</param>
         public static string RenderTable(string[,] Rows, int left, int top, int width, int height, bool enableHeader, Color SeparatorForegroundColor, Color HeaderForegroundColor, Color ValueForegroundColor, Color BackgroundColor, List<CellOptions>? CellOptions = null, BorderSettings? tableBorderSettings = null) =>
-            RenderTable(Rows, left, top, width, height, enableHeader, SeparatorForegroundColor, HeaderForegroundColor, ValueForegroundColor, BackgroundColor, true, CellOptions, tableBorderSettings);
-
-        /// <summary>
-        /// Renders a table with text
-        /// </summary>
-        /// <param name="Rows">Rows to insert to the table.</param>
-        /// <param name="left">Left position of the upper-left corner</param>
-        /// <param name="top">Top position of the upper-left corner</param>
-        /// <param name="width">Table interior width</param>
-        /// <param name="height">Table interior height</param>
-        /// <param name="enableHeader">Whether to enable the header or no</param>
-        /// <param name="CellOptions">Specifies the cell options</param>
-        /// <param name="SeparatorForegroundColor">A separator foreground color that will be changed to.</param>
-        /// <param name="HeaderForegroundColor">A header foreground color that will be changed to.</param>
-        /// <param name="ValueForegroundColor">A value foreground color that will be changed to.</param>
-        /// <param name="BackgroundColor">A background color that will be changed to.</param>
-        /// <param name="useColor">Whether to use the colors or not</param>
-        /// <param name="tableBorderSettings">Specifies the table border settings</param>
-        internal static string RenderTable(string[,] Rows, int left, int top, int width, int height, bool enableHeader, Color SeparatorForegroundColor, Color HeaderForegroundColor, Color ValueForegroundColor, Color BackgroundColor, bool useColor, List<CellOptions>? CellOptions = null, BorderSettings? tableBorderSettings = null)
-        {
-            // Create a border which the table will be drawn on
-            var tableBuilder = new StringBuilder();
-            tableBorderSettings ??= new();
-
-            // Determine the positions
-            int columnsCount = Rows.GetLength(1);
-            int rowsCount = Rows.GetLength(0);
-            (int, int)[,] positions = new (int, int)[columnsCount, rowsCount];
-            int maxCellWidth = (width + 2) / columnsCount;
-            for (int x = 0; x < columnsCount; x++)
-            {
-                for (int y = 0; y < rowsCount; y++)
-                {
-                    int finalPosX = left + maxCellWidth * x + 1;
-                    int finalPosY = top + y + 1;
-                    if (enableHeader && y > 0)
-                        finalPosY++;
-                    positions[x, y] = (finalPosX, finalPosY);
-                }
-            }
-
-            // Use the final width to create the actual table
-            if (useColor)
-            {
-                tableBuilder.Append(
-                    BorderColor.RenderBorder(left, top, maxCellWidth * columnsCount - 1, height, tableBorderSettings, SeparatorForegroundColor, BackgroundColor) +
-                    ColorTools.RenderSetConsoleColor(SeparatorForegroundColor) +
-                    ColorTools.RenderSetConsoleColor(BackgroundColor, true)
-                );
-            }
-            else
-            {
-                tableBuilder.Append(
-                    BorderColor.RenderBorderPlain(left, top, maxCellWidth * columnsCount - 1, height, tableBorderSettings)
-                );
-            }
-
-            // Create a header separator if we need a header
-            if (enableHeader)
-            {
-                char begin = tableBorderSettings.BorderLeftHorizontalIntersectionChar;
-                char middle = tableBorderSettings.BorderHorizontalIntersectionChar;
-                char end = tableBorderSettings.BorderRightHorizontalIntersectionChar;
-                int headerBorderPosX = left;
-                int headerBorderPosY = top + 2;
-                tableBuilder.Append(
-                    CsiSequences.GenerateCsiCursorPosition(headerBorderPosX + 1, headerBorderPosY + 1) +
-                    begin + new string(middle, maxCellWidth * columnsCount - 1) + end
-                );
-            }
-
-            // Create a row separator
-            char beginVertical = tableBorderSettings.BorderTopVerticalIntersectionChar;
-            char middleVertical = tableBorderSettings.BorderVerticalIntersectionChar;
-            char endVertical = tableBorderSettings.BorderBottomVerticalIntersectionChar;
-            char intersect = tableBorderSettings.BorderWholeIntersectionChar;
-            for (int x = 1; x < columnsCount; x++)
-            {
-                // Try to get the positions for the separator
-                var positionsSeparator = ((int, int))positions.GetValue(x, 0);
-
-                // Build the separator
-                for (int y = 0; y < height + 2; y++)
-                {
-                    char finalChar =
-                        y == 0 ? beginVertical :
-                        y == height + 1 ? endVertical :
-                        y == 2 && enableHeader ? intersect :
-                        middleVertical;
-                    tableBuilder.Append(
-                        CsiSequences.GenerateCsiCursorPosition(positionsSeparator.Item1, y + top + 1) +
-                        finalChar
-                    );
-                }
-            }
-
-            // Write values
-            for (int y = 0; y < rowsCount; y++)
-            {
-                for (int x = 0; x < columnsCount; x++)
-                {
-                    // Get the initial values
-                    var positionsValues = ((int, int))positions.GetValue(x, y);
-                    if (positionsValues.Item2 > height + top)
-                        break;
-                    string text = (string)Rows.GetValue(y, x);
-                    Color finalColor =
-                        y == 0 && enableHeader ? HeaderForegroundColor :
-                        ValueForegroundColor;
-                    Color finalBackgroundColor = BackgroundColor;
-
-                    // Process them according to both the cell width and the cell options
-                    string spaces = new(' ', maxCellWidth - 1);
-                    text = text.Truncate(maxCellWidth - 4);
-                    var alignment = TextAlignment.Left;
-                    if (CellOptions is not null && CellOptions.Count > 0)
-                    {
-                        var options = CellOptions.FirstOrDefault((co) => co.ColumnIndex == x && co.RowIndex == y);
-                        if (options is not null && options.ColoredCell)
-                        {
-                            if (options.ColoredCell)
-                            {
-                                finalColor = options.CellColor;
-                                finalBackgroundColor = options.CellBackgroundColor;
-                            }
-                            alignment = options.TextSettings.Alignment;
-                        }
-                    }
-
-                    // Adjust the X position according to the alignment
-                    int alignmentPosX = TextWriterTools.DetermineTextAlignment(text, maxCellWidth - 1, alignment, positionsValues.Item1);
-
-                    // Write them
-                    if (useColor)
-                    {
-                        tableBuilder.Append(
-                            TextWriterWhereColor.RenderWhereColorBack(spaces, positionsValues.Item1, positionsValues.Item2, finalColor, finalBackgroundColor) +
-                            TextWriterWhereColor.RenderWhereColorBack(text, alignmentPosX, positionsValues.Item2, finalColor, finalBackgroundColor)
-                        );
-                    }
-                    else
-                    {
-                        tableBuilder.Append(
-                            TextWriterWhereColor.RenderWhere(spaces, positionsValues.Item1, positionsValues.Item2) +
-                            TextWriterWhereColor.RenderWhere(text, alignmentPosX, positionsValues.Item2)
-                        );
-                    }
-                }
-            }
-
-            // Final results are here
-            return tableBuilder.ToString();
-        }
+            Table.RenderTable(Rows, left, top, width, height, enableHeader, SeparatorForegroundColor, HeaderForegroundColor, ValueForegroundColor, BackgroundColor, true, CellOptions, tableBorderSettings);
 
         static TableColor()
         {
