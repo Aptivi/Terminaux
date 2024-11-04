b175e1e9e9a7201eb8ebfb74aac03656b25616ee
Aptivi CEO <eoflaoevicecity@gmail.com>
2024-11-04 19:53:23 +0300

    add - Added scaffolding for markup support
    ---
    
    We've added two class placeholders to implement markup support that is
    going to be fully compatible with the Spectre.Console syntax.
    
    ---
    
    Type: add
    Breaking: False
    Doc Required: False
    Backport Required: False
    Part: 1/1

---
+++ Terminaux/Writer/CyclicWriters/Renderer/Markup/MarkupTools.cs
@@ -0,0 +1,28 @@
+﻿//
+// Terminaux  Copyright (C) 2023-2024  Aptivi
+//
+// This file is part of Terminaux
+//
+// Terminaux is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Terminaux is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY, without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+
+namespace Terminaux.Writer.CyclicWriters.Renderer.Markup
+{
+    /// <summary>
+    /// Markup parsing and processing tools
+    /// </summary>
+    public static class MarkupTools
+    {
+    }
+}
