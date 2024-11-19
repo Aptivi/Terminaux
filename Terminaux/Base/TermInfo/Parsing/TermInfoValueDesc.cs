a667f0ae7e43b5d86e668901bbcc5ba688218729
Aptivi <eoflaoevicecity@gmail.com>
2024-11-19 13:18:17 +0300

    imp - Added sequence rendering
    ---
    
    We've added sequence rendering with delay support as explained in
    https://man7.org/linux/man-pages/man5/terminfo.5.html
    
    ---
    
    Type: imp
    Breaking: False
    Doc Required: False
    Backport Required: False
    Part: 1/1

--- Terminaux/Base/TermInfo/Parsing/TermInfoValueDesc.cs
+++ Terminaux/Base/TermInfo/Parsing/TermInfoValueDesc.cs
@@ -17,7 +17,10 @@
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 //
 
+using System.Collections.Generic;
 using System.Diagnostics;
+using System.Text;
+using System.Threading;
 using Terminaux.Base.TermInfo.Parsing.Parameters;
 using Terminaux.Writer.ConsoleWriters;
 
@@ -75,6 +78,76 @@ namespace Terminaux.Base.TermInfo.Parsing
             // Now, process the sequence with the provided arguments
             string processed = ParameterProcessor.ProcessSequenceParams(valueDesc, args);
             return processed;
+        }
+
+        /// <summary>
+        /// Renders the sequence with delay support
+        /// </summary>
+        /// <param name="args">Arguments to pass to the sequence processor</param>
+        /// <exception cref="TerminauxException"></exception>
+        public void RenderSequence(params object?[]? args)
+        {
+            // Process the sequence
+            string processed = ProcessSequence(args) ?? "";
+
+            // Find the delay indicators
+            List<(int, string)> parts = [];
+            StringBuilder partBuilder = new();
+            int delay = 0;
+            for (int i = 0; i < processed.Length; i++)
+            {
+                // Check to see if we're at the delay indicator
+                char ch = processed[i];
+                if (ch == '$')
+                {
+                    char next = i + 1 < processed.Length ? processed[i + 1] : '\0';
+                    if (next == '<')
+                    {
+                        StringBuilder delayBuilder = new();
+                        i++;
+                        while (true)
+                        {
+                            if (++i == processed.Length)
+                                throw new TerminauxException("Delay designator ended prematurely.");
+
+                            // Add the digits or flags, as long as they're valid
+                            next = processed[i];
+                            if (next == '>')
+                                break;
+                            if (!char.IsNumber(next) && next != '*' && next != '/')
+                                throw new TerminauxException("Invalid delay designator.");
+
+                            // Ignore the flags
+                            if (next == '*' || next == '/')
+                                continue;
+
+                            // Add the digit
+                            delayBuilder.Append(next);
+                        }
+
+                        // Convert to integer and add the result, clearing it in the process.
+                        if (!int.TryParse(delayBuilder.ToString(), out delay))
+                            throw new TerminauxException("Delay value is not a number.");
+                        parts.Add((delay, partBuilder.ToString()));
+                        partBuilder.Clear();
+                        delay = 0;
+                        continue;
+                    }
+                }
+
+                // Now, add the character as it is
+                partBuilder.Append(ch);
+            }
+            if (partBuilder.Length > 0)
+                parts.Add((delay, partBuilder.ToString()));
+
+            // Now, render the sequence with delays
+            foreach ((int seqDelay, string sequence) in parts)
+            {
+                TextWriterRaw.WriteRaw(sequence);
+                if (seqDelay > 0)
+                    Thread.Sleep(seqDelay);
+            }
         }
 
         internal TermInfoValueDesc(T? value, string name, TermInfoValueType valueType, ParameterInfo[]? parameters)
