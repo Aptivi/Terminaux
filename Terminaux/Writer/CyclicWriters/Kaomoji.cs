fe508fbbb59908091a4f37663c8caa3eb183465a
Aptivi <eoflaoevicecity@gmail.com>
2024-11-20 11:32:14 +0300

    add - doc - Added Kaomoji writer
    ---
    
    We've added the Kaomoji renderable
    
    ---
    
    Type: add
    Breaking: False
    Doc Required: True
    Backport Required: False
    Part: 1/1

---
+++ Terminaux/Writer/CyclicWriters/Kaomoji.cs
@@ -0,0 +1,47 @@
+﻿//
+// Terminaux  Copyright (C) 2023-2024  Aptivi
+//
+// This file is part of Terminaux
+//
+// Terminaux is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Terminaux is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY, without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+
+using Textify.Data.Unicode;
+
+namespace Terminaux.Writer.CyclicWriters
+{
+    /// <summary>
+    /// Kaomoji renderer
+    /// </summary>
+    public class Kaomoji : IStaticRenderable
+    {
+        private readonly string kaomoji = "";
+
+        /// <summary>
+        /// Renders kaomoji
+        /// </summary>
+        /// <returns>A string representation of the kaomoji</returns>
+        public string Render() =>
+            kaomoji;
+
+        /// <summary>
+        /// Makes a new kaomoji instance
+        /// </summary>
+        /// <param name="category">Kaomoji category</param>
+        /// <param name="subcategory">Kaomoji subcategory</param>
+        /// <param name="idx">Kaomoji index</param>
+        public Kaomoji(KaomojiCategory category, KaomojiSubcategory subcategory, int idx) =>
+            kaomoji = KaomojiManager.GetKaomoji(category, subcategory, idx);
+    }
+}
