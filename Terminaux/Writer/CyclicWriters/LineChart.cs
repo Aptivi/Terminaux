a52e4aa6c42ef368ab5b4c4495ae79d73f7a54bc
Aptivi <eoflaoevicecity@gmail.com>
2024-11-26 12:40:01 +0300

    add - Added scaffoldings for different chart types
    ---
    
    We've added new chart types that allow you to describe data in various
    ways. This is just scaffolding code, though.
    
    ---
    
    Type: add
    Breaking: False
    Doc Required: False
    Backport Required: False
    Part: 1/1

---
+++ Terminaux/Writer/CyclicWriters/LineChart.cs
@@ -0,0 +1,143 @@
+﻿//
+// Terminaux  Copyright (C) 2023-2024  Aptivi
+//
+// This file is part of Terminaux
+//
+// Terminaux is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Terminaux is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY, without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+
+using System;
+using System.Linq;
+using System.Text;
+using Terminaux.Base.Extensions;
+using Terminaux.Colors;
+using Terminaux.Colors.Data;
+using Terminaux.Writer.ConsoleWriters;
+using Terminaux.Writer.CyclicWriters.Renderer.Tools;
+
+namespace Terminaux.Writer.CyclicWriters
+{
+    /// <summary>
+    /// Line chart renderable
+    /// </summary>
+    public class LineChart : IStaticRenderable
+    {
+        private ChartElement[] elements = [];
+        private int left = 0;
+        private int top = 0;
+        private int interiorWidth = 0;
+        private int interiorHeight = 0;
+        private bool showcase = false;
+        private bool run = false;
+        private bool useColors = true;
+
+        /// <summary>
+        /// Left position
+        /// </summary>
+        public int Left
+        {
+            get => left;
+            set => left = value;
+        }
+
+        /// <summary>
+        /// Top position
+        /// </summary>
+        public int Top
+        {
+            get => top;
+            set => top = value;
+        }
+
+        /// <summary>
+        /// Interior width
+        /// </summary>
+        public int InteriorWidth
+        {
+            get => interiorWidth;
+            set => interiorWidth = value;
+        }
+
+        /// <summary>
+        /// Interior height
+        /// </summary>
+        public int InteriorHeight
+        {
+            get => interiorHeight;
+            set => interiorHeight = value;
+        }
+
+        /// <summary>
+        /// Show the element list
+        /// </summary>
+        public bool Showcase
+        {
+            get => showcase;
+            set => showcase = value;
+        }
+
+        /// <summary>
+        /// Whether to render this chart as a run chart
+        /// </summary>
+        public bool RunChart
+        {
+            get => run;
+            set => run = value;
+        }
+
+        /// <summary>
+        /// Whether to use colors or not
+        /// </summary>
+        public bool UseColors
+        {
+            get => useColors;
+            set => useColors = value;
+        }
+
+        /// <summary>
+        /// Chart elements
+        /// </summary>
+        public ChartElement[] Elements
+        {
+            get => elements;
+            set => elements = value;
+        }
+
+        /// <summary>
+        /// Renders a line chart
+        /// </summary>
+        /// <returns>Rendered text that will be used by the renderer</returns>
+        public string Render()
+        {
+            return TextWriterWhereColor.RenderWhere(
+                RenderLineChart(
+                    elements, InteriorWidth, InteriorHeight, Showcase, RunChart, UseColors), Left, Top);
+        }
+
+        internal static string RenderLineChart(ChartElement[] elements, int InteriorWidth, int InteriorHeight, bool showcase = false, bool run = false, bool useColor = true)
+        {
+            StringBuilder lineChart = new();
+
+            // TODO: This is just a scaffolding code.
+            // Return the result
+            return lineChart.ToString();
+        }
+
+        /// <summary>
+        /// Makes a new instance of the line chart renderer
+        /// </summary>
+        public LineChart()
+        { }
+    }
+}
