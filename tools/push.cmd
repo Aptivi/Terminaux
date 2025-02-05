efc27208c3a3b84a5245a7b27ecae74419cf3a68
Aptivi <eoflaoevicecity@gmail.com>
2025-02-05 13:29:09 +0300

    int - Roll out CI builds
    ---
    
    This is not done yet!
    
    ---
    
    Type: int
    Breaking: False
    Doc Required: False
    Backport Required: False
    Part: 1/1

--- tools/push.cmd
+++ tools/push.cmd
@@ -1,27 +1,11 @@
 @echo off
-
-REM    Terminaux  Copyright (C) 2023-2025  Aptivi
-REM
-REM    This file is part of Terminaux
-REM
-REM    Terminaux is free software: you can redistribute it and/or modify
-REM    it under the terms of the GNU General Public License as published by
-REM    the Free Software Foundation, either version 3 of the License, or
-REM    (at your option) any later version.
-REM
-REM    Terminaux is distributed in the hope that it will be useful,
-REM    but WITHOUT ANY WARRANTY; without even the implied warranty of
-REM    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-REM    GNU General Public License for more details.
-REM
-REM    You should have received a copy of the GNU General Public License
-REM    along with this program.  If not, see <https://www.gnu.org/licenses/>.
-
 set apikey=%1
+set source=%2
+if "%source%" == "" set source=nuget.org
 
 REM This script pushes. Use when you have VS installed.
 echo Pushing...
-cmd /C "forfiles /s /m *.nupkg /p ..\ /C "cmd /c dotnet nuget push @path --api-key %apikey% --source nuget.org""
+cmd /C "forfiles /s /m *.nupkg /p ..\ /C "cmd /c dotnet nuget push @path --api-key %apikey% --source %source%""
 if %errorlevel% == 0 goto :success
 echo There was an error trying to push (%errorlevel%).
 goto :finished
