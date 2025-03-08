02f68f049081dd63fcddabfeb96c2bf2d34f1596
Aptivi CEO <eoflaoevicecity@gmail.com>
2025-03-08 15:47:51 +0300

    bkp - Backported build system improvements
    ---
    
    We've backported a series of build system improvements from Aptivestigate
    as it has the most up-to-date files.
    
    ---
    
    The following commits or tags are used for this backport:
      - Aptivi/Aptivestigate@93569e6
      - Aptivi/Aptivestigate@6ffb18b
      - Aptivi/Aptivestigate@a0a12ea
      - Aptivi/Aptivestigate@054d8b1
      - Aptivi/Aptivestigate@bac13b7
      - Aptivi/Aptivestigate@a7e3771
      - Aptivi/Aptivestigate@a1d33fe
      - Aptivi/Aptivestigate@835b3f1
    
    ---
    
    Type: bkp
    Breaking: False
    Doc Required: False
    Backport Required: False
    Part: 1/1

--- tools/docgen.sh
+++ tools/docgen.sh
@@ -1,22 +1,5 @@
 #!/bin/bash
 
-#   Terminaux  Copyright (C) 2023-2025  Aptivi
-# 
-#   This file is part of Terminaux
-# 
-#   Terminaux is free software: you can redistribute it and/or modify
-#   it under the terms of the GNU General Public License as published by
-#   the Free Software Foundation, either version 3 of the License, or
-#   (at your option) any later version.
-# 
-#   Terminaux is distributed in the hope that it will be useful,
-#   but WITHOUT ANY WARRANTY; without even the implied warranty of
-#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#   GNU General Public License for more details.
-# 
-#   You should have received a copy of the GNU General Public License
-#   along with this program.  If not, see <https://www.gnu.org/licenses/>.
-
 # Repository root
 ROOTDIR=$( cd -- "$( dirname -- "$0" )/.." &> /dev/null && pwd )
 
@@ -31,7 +14,7 @@ fi
 export DOTNET_CLI_TELEMETRY_OPTOUT=1
 export DOTNET_NOLOGO=1
 
-# Build KS
+# Build docs
 echo Building documentation...
 docfx $ROOTDIR/DocGen/docfx.json
 if [ ! $? == 0 ]; then
