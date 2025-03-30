612f0a2858afd81e73456dc3bce7c4eebe1b336d
Aptivi CEO <eoflaoevicecity@gmail.com>
2025-03-30 17:33:52 +0300

    imp - Added property checking for Asciinema
    ---
    
    In the unit tests of the Asciinema parsing function, we've added property
    correctness checking.
    
    ---
    
    Type: imp
    Breaking: False
    Doc Required: False
    Backport Required: False
    Part: 1/1

--- private/Terminaux.Tests/Asciinema/AsciinemaTests.cs
+++ private/Terminaux.Tests/Asciinema/AsciinemaTests.cs
@@ -62,9 +62,32 @@ namespace Terminaux.Tests.Asciinema
 
             // Run the representation parser
             var parsed = AsciinemaRepresentation.GetRepresentationFromContents(representation);
+
+            // Test general properties
             parsed.Version.ShouldBe(1);
             parsed.Width.ShouldBe(80);
             parsed.Height.ShouldBe(24);
+
+            // Test type correctness
+            parsed.Asciicast.ShouldNotBeNull();
+            parsed.Asciicast.ShouldBeOfType<AsciicastV1>();
+
+            // Test individual properties
+            var asciiCastV1 = parsed.Asciicast as AsciicastV1;
+            asciiCastV1?.Duration.ShouldBe(1.515658f);
+            asciiCastV1?.Command.ShouldBe("/bin/zsh");
+            asciiCastV1?.Title.ShouldBe("");
+            asciiCastV1?.Environment.ShouldNotBeNull();
+            asciiCastV1?.Environment?.Terminal.ShouldBe("xterm-256color");
+            asciiCastV1?.Environment?.Shell.ShouldBe("/bin/zsh");
+            asciiCastV1?.StdOutData.ShouldNotBeEmpty();
+            asciiCastV1?.StdOutData.Count.ShouldBe(2);
+            asciiCastV1?.StdOutData[0].Item1.ShouldBe(0.248848);
+            asciiCastV1?.StdOutData[0].Item2.ShouldBe("o");
+            asciiCastV1?.StdOutData[0].Item3.ShouldBe("\u001b[1;31mHello \u001b[32mWorld!\u001b[0m\n");
+            asciiCastV1?.StdOutData[1].Item1.ShouldBe(1.001376);
+            asciiCastV1?.StdOutData[1].Item2.ShouldBe("o");
+            asciiCastV1?.StdOutData[1].Item3.ShouldBe("I am \rThis is on the next line.");
         }
 
         /// <summary>
@@ -88,9 +111,47 @@ namespace Terminaux.Tests.Asciinema
 
             // Run the representation parser
             var parsed = AsciinemaRepresentation.GetRepresentationFromContents(representation);
+
+            // Test general properties
             parsed.Version.ShouldBe(2);
             parsed.Width.ShouldBe(80);
             parsed.Height.ShouldBe(24);
+
+            // Test type correctness
+            parsed.Asciicast.ShouldNotBeNull();
+            parsed.Asciicast.ShouldBeOfType<AsciicastV2>();
+
+            // Test individual properties
+            var asciiCastV2 = parsed.Asciicast as AsciicastV2;
+            asciiCastV2?.Duration.ShouldBe(0);
+            asciiCastV2?.Timestamp.ShouldBe(1504467315);
+            asciiCastV2?.IdleTimeLimit.ShouldBe(0);
+            asciiCastV2?.Command.ShouldBe("");
+            asciiCastV2?.Title.ShouldBe("Demo");
+            asciiCastV2?.Environment.ShouldNotBeNull();
+            asciiCastV2?.Environment?.Terminal.ShouldBe("xterm-256color");
+            asciiCastV2?.Environment?.Shell.ShouldBe("/bin/zsh");
+            asciiCastV2?.Theme.ShouldBeNull();
+            asciiCastV2?.StdOutData.ShouldNotBeEmpty();
+            asciiCastV2?.StdOutData.Count.ShouldBe(6);
+            asciiCastV2?.StdOutData[0].Item1.ShouldBe(0.248848);
+            asciiCastV2?.StdOutData[0].Item2.ShouldBe("o");
+            asciiCastV2?.StdOutData[0].Item3.ShouldBe("\u001b[1;31mHello \u001b[32mWorld!\u001b[0m\n");
+            asciiCastV2?.StdOutData[1].Item1.ShouldBe(1.001376);
+            asciiCastV2?.StdOutData[1].Item2.ShouldBe("o");
+            asciiCastV2?.StdOutData[1].Item3.ShouldBe("That was ok\rThis is better.");
+            asciiCastV2?.StdOutData[2].Item1.ShouldBe(1.500000);
+            asciiCastV2?.StdOutData[2].Item2.ShouldBe("m");
+            asciiCastV2?.StdOutData[2].Item3.ShouldBe("");
+            asciiCastV2?.StdOutData[3].Item1.ShouldBe(2.143733);
+            asciiCastV2?.StdOutData[3].Item2.ShouldBe("o");
+            asciiCastV2?.StdOutData[3].Item3.ShouldBe("Now... ");
+            asciiCastV2?.StdOutData[4].Item1.ShouldBe(4.050000);
+            asciiCastV2?.StdOutData[4].Item2.ShouldBe("r");
+            asciiCastV2?.StdOutData[4].Item3.ShouldBe("80x24");
+            asciiCastV2?.StdOutData[5].Item1.ShouldBe(6.541828);
+            asciiCastV2?.StdOutData[5].Item2.ShouldBe("o");
+            asciiCastV2?.StdOutData[5].Item3.ShouldBe("Bye!");
         }
     }
 }
