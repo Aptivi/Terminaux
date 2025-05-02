d8195066edde52890cbdf563599c7a5be9a6c542
Aptivi CEO <eoflaoevicecity@gmail.com>
2025-05-02 10:33:00 +0300

    int - Rollout build script improvements
    ---
    
    Type: int
    Breaking: False
    Doc Required: False
    Backport Required: False
    Part: 1/1

--- vnd/vendor-docpack.cmd
+++ vnd/vendor-docpack.cmd
@@ -3,12 +3,12 @@
 set ROOTDIR=%~dp0\..
 
 REM This script builds the documentation and packs the artifacts.
-for /f "tokens=*" %%g in ('findstr "<Version>" %ROOTDIR%\Directory.Build.props') do (set MIDVER=%%g)
+for /f "tokens=*" %%g in ('findstr "<Version>" "%ROOTDIR%\Directory.Build.props"') do (set MIDVER=%%g)
 for /f "tokens=1 delims=<" %%a in ("%MIDVER:~9%") do (set version=%%a)
 
 :pack
 echo Packing documentation...
-"%ProgramFiles%\7-Zip\7z.exe" a -tzip %temp%/%version%-doc.zip "%ROOTDIR%\docs\*"
+"%ProgramFiles%\7-Zip\7z.exe" a -tzip "%temp%/%version%-doc.zip" "%ROOTDIR%\docs\*"
 if %errorlevel% == 0 goto :finalize
 echo There was an error trying to pack documentation (%errorlevel%).
 goto :finished
@@ -17,6 +17,6 @@ goto :finished
 rmdir /S /Q "%ROOTDIR%\DocGen\api\"
 rmdir /S /Q "%ROOTDIR%\DocGen\obj\"
 rmdir /S /Q "%ROOTDIR%\docs\"
-move %temp%\%version%-doc.zip %ROOTDIR%\tools\
+move "%temp%\%version%-doc.zip" "%ROOTDIR%\tools\"
 
 :finished
