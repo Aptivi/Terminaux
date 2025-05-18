98caa8a6128d36f54d73001e109d288517ab2026
Aptivi CEO <eoflaoevicecity@gmail.com>
2025-05-18 17:20:51 +0300

    add - doc - Added InfoBoxSettings (to be used)
    ---
    
    We'll use this type later. We'll deprecate the legacy functions from now
    to make space for the modern function declaration that uses this class to
    avoid conflicts later.
    
    In the final release, those functions, which will be marked with
    [Obsolete], will be deleted from the final API.
    
    ---
    
    Type: add
    Breaking: False
    Doc Required: True
    Backport Required: False
    Part: 1/1

---
+++ public/Terminaux/Inputs/Styles/Infobox/Tools/InfoBoxSettings.cs
@@ -0,0 +1,103 @@
+﻿//
+// Terminaux  Copyright (C) 2023-2025  Aptivi
+//
+// This file is part of Terminaux
+//
+// Terminaux is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Terminaux is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY, without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+
+using Terminaux.Colors;
+using Terminaux.Colors.Data;
+using Terminaux.Writer.CyclicWriters.Renderer.Tools;
+
+namespace Terminaux.Inputs.Styles.Infobox.Tools
+{
+    /// <summary>
+    /// Infobox settings
+    /// </summary>
+    public class InfoBoxSettings
+    {
+        private static readonly InfoBoxSettings globalSettings = new();
+        internal bool useColors = true;
+        internal string title = "";
+        internal BorderSettings borderSettings = BorderSettings.GlobalSettings;
+        internal Color foregroundColor = new(ConsoleColors.Silver);
+        internal Color backgroundColor = ColorTools.currentBackgroundColor;
+
+        /// <summary>
+        /// Global infobox settings
+        /// </summary>
+        public static InfoBoxSettings GlobalSettings =>
+            globalSettings;
+
+        /// <summary>
+        /// Title of the informational box 
+        /// </summary>
+        public string Title
+        {
+            get => title;
+            set => title = value;
+        }
+        /// <summary>
+        /// Border settings for the informational box 
+        /// </summary>
+        public BorderSettings BorderSettings
+        {
+            get => borderSettings;
+            set => borderSettings = value;
+        }
+        /// <summary>
+        /// Foreground color of the infobox 
+        /// </summary>
+        public Color ForegroundColor
+        {
+            get => foregroundColor;
+            set => foregroundColor = value;
+        }
+        /// <summary>
+        /// Background color of the infobox 
+        /// </summary>
+        public Color BackgroundColor
+        {
+            get => backgroundColor;
+            set => backgroundColor = value;
+        }
+        /// <summary>
+        /// Whether to use the colors or not 
+        /// </summary>
+        public bool UseColors
+        {
+            get => useColors;
+            set => useColors = value;
+        }
+
+        /// <summary>
+        /// Makes a new instance of the infobox settings
+        /// </summary>
+        public InfoBoxSettings()
+        { }
+
+        /// <summary>
+        /// Makes a new instance of the infobox settings with the copied settings
+        /// </summary>
+        /// <param name="settings">Settings instance to copy settings from</param>
+        public InfoBoxSettings(InfoBoxSettings settings)
+        {
+            title = settings.title;
+            borderSettings = settings.borderSettings;
+            foregroundColor = settings.foregroundColor;
+            backgroundColor = settings.backgroundColor;
+        }
+    }
+}
