812b07265bdf5c7db6e24f7b0baee8657a3d47b1
Aptivi CEO <eoflaoevicecity@gmail.com>
2025-06-06 20:10:27 +0300

    int - Added incrementor script
    ---
    
    Type: int
    Breaking: False
    Doc Required: False
    Backport Required: False
    Part: 1/1

---
+++ vnd/vendor-increment.cmd
@@ -0,0 +1,24 @@
+@echo off
+
+set oldVer=%1
+set newVer=%2
+if "%oldVer%" == "" (
+    echo Old version must be specified.
+    exit /b 1
+)
+if "%newVer%" == "" (
+    echo New version must be specified to replace old version "%oldVer%".
+    exit /b 1
+)
+
+set ROOTDIR=%~dp0..
+
+REM This script replaces old version with new version.
+echo Replacing...
+
+REM Change the below files
+set "releaseFiles=%ROOTDIR%\Directory.Build.props %ROOTDIR%\CHANGES.TITLE"
+for %%f in (%releaseFiles%) do (
+    echo Processing %%f...
+    powershell %ROOTDIR%\vnd\eng\incrementor.ps1 "%%f" "%oldVer%" "%newVer%"
+)
