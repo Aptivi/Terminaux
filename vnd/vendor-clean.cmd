2bfc3cde17fa801a8e310d4515cdc79a617f733d
Aptivi CEO <eoflaoevicecity@gmail.com>
2026-03-05 16:46:16 +0300

    rem - Removed old vendor script files
    ---
    
    We're no longer using the older script-based toolkit.
    
    ---
    
    Type: rem
    Breaking: No
    Doc Required: No
    Backport Required: No
    AI Assisted: No
    Part: 1/1

--- vnd/vendor-clean.cmd
+++
@@ -1,6 +0,0 @@
-@echo off
-
-set ROOTDIR=%~dp0..
-
-echo Cleaning up...
-for %%f in (bin, obj, docs, Generated) do forfiles /s /m %%f /p "%ROOTDIR%" /c "cmd /c if @isdir==TRUE (echo @path && rd /s /q @path)" 2>nul
