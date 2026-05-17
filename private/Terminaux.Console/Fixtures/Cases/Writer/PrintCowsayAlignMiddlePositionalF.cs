92fecb0f15a9ad3afcf747f97be3886a61a3bcf2
Aptivi CEO <eoflaoevicecity@gmail.com>
2026-05-17 11:19:39 +0300

    add - Added positional and aligned cowsay cases
    ---
    
    We've added test cases for positional and aligned cowsay cases.
    
    ---
    
    Type: add
    Breaking: No
    Doc Required: No
    Backport Required: No
    AI Assisted: No
    Part: 1/1

---
+++ private/Terminaux.Console/Fixtures/Cases/Writer/PrintCowsayAlignMiddlePositionalF.cs
@@ -0,0 +1,44 @@
+﻿//
+// Terminaux  Copyright (C) 2023-2026  Aptivi
+//
+// This file is part of Terminaux
+//
+// Terminaux is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Terminaux is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY, without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+
+using Terminaux.Base.Extensions;
+using Terminaux.Writer.ConsoleWriters;
+using Terminaux.Writer.CyclicWriters.Graphical;
+using Terminaux.Writer.CyclicWriters.Renderer.Tools;
+
+namespace Terminaux.Console.Fixtures.Cases.Writer
+{
+    internal class PrintCowsayAlignMiddlePositionalF : IFixture
+    {
+        public FixtureCategory Category => FixtureCategory.Writer;
+
+        public void RunFixture()
+        {
+            ConsoleColoring.LoadBack();
+            var cowsay = new AlignedCowsayText(CowName.Default, "Hello {0}!", "world")
+            {
+                Settings = new() { Alignment = TextAlignment.Middle },
+                Top = 5,
+                Left = 20,
+                Margins = new(0, 0, 30, 0)
+            };
+            TextWriterRaw.WriteRaw(cowsay.Render());
+        }
+    }
+}
