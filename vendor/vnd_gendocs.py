910d11510f45664a69cd536420bcc9fba765d605
Aptivi CEO <eoflaoevicecity@gmail.com>
2026-06-20 14:03:26 +0300

    fix - Fixed paths with spaces causing failures
    ---
    
    With the vendor scripts, we've fixed false failures in some scripts caused
    by paths with spaces.
    
    ---
    
    Type: fix
    Breaking: No
    Doc Required: No
    Backport Required: No
    AI Assisted: No
    Part: 1/1

--- vendor/vnd_gendocs.py
+++ vendor/vnd_gendocs.py
@@ -4,7 +4,7 @@ import subprocess
 
 def vnd_gendocs(extra_args):
     solution = os.path.dirname(os.path.abspath(__file__ + '/../'))
-    command = f"docfx {solution}/DocGen/docfx.json"
+    command = f"docfx \"{solution}/DocGen/docfx.json\""
     result = subprocess.run(command, shell=True)
     if result.returncode != 0:
         raise Exception("Generation failed with code %i" % (result.returncode))
